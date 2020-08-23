0e6f5e0c687111edc01c09dc303f144f52603175
zcareyltu <38868466+zcareyltu@users.noreply.github.com>
2020-08-23 15:50:41 -0400

    Snake collisions!

--- snake.js
+++ snake.js
@@ -112,23 +112,27 @@ function game(){
         velY = inputY;
     }
 
+    //Move the head and check for collisions
     posX += velX;
     posY += velY;
-    trail.push({
-        x: posX,
-        y: posY
-    });
 
+    //Only remove the tail of the trail if we didn't hit an apple, otherwise move the apple
     if(posX == appleX && posY == appleY){
         randomApplePosition();
     }else{
         trail.shift();
     }
     
-    if((posX < 0) || (posX >= mapWidth) || (posY < 0) || (posY >= mapHeight)){
+    if(((velX != 0 || velY != 0) && isSnakeColliding()) || (posX < 0) || (posX >= mapWidth) || (posY < 0) || (posY >= mapHeight)){
         loseGame();
         return;
     }
+
+    //Add the new position to the trail.
+    trail.push({
+        x: posX,
+        y: posY
+    });
 
     graphics.copyImage(bgCanvas);
     drawApple();
@@ -161,6 +165,15 @@ function randomApplePosition(){
     if(appleX == posX && appleY == posY){
         appleX = (appleX + 1).clamp(0, mapWidth - 1);
     }
+}
+
+function isSnakeColliding(){
+    for(var i in trail){
+        if(trail[i].x == posX && trail[i].y == posY){
+            return true;
+        }
+    }
+    return false;
 }
 
 function drawSnake(){
@@ -328,51 +341,3 @@ function applySettingsClick() {
 
 initialize();
 gameReset();
-
-/*
-    px=py=10;
-    gs=tc=20;
-    ax=ay=15;
-    xv = yv = 0;
-    trail=[];
-    tail =5;
-    function game() {
-        px+=xv;
-        py+=yv;
-        if(px<0){
-            px=tc-1;
-        }
-        if(px>tc-1){
-            px=0;
-        }
-        if(py<0){
-            py=tc-1;
-        }
-        if(py>tc-1){
-            py=0;
-        }
-        ctx.fillStyle="black";
-        ctx.fillRect(0,0,canv.width,canv.height);
-
-        ctx.fillStyle="lime";
-        for(var i=0; i<trail.length;i++){
-            ctx.fillRect(trail[i].x*gs,trail[i].y*gs,gs-2,gs-2);
-            if(trail[i].x==px && trail[i].y==py){
-                tail=5;
-            }
-        }
-        trail.push({x:px, y:py});
-        while(trail.length>tail){
-            trail.shift();
-        }
-
-        if(ax==px && ay==py){
-                tail++;
-                ax=Math.floor(Math.random()*tc);
-                ay=Math.floor(Math.random()*tc);
-            }
-
-        ctx.fillStyle="red";
-        ctx.fillRect(ax*gs,ay*gs,gs-2,gs-2);
-    }
-*/
